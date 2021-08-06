e1dd7e7995253fad10031519e5ade9f914c546ba
Daria Krestina <dariakrestina@Darias-MacBook.local>
2021-08-06 10:55:57 -0400

    P1 submission

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-// Make a request from open weather api api.openweathermap.org/data/2.5/weather?q={city}&appid=4da7e530effb30d411db83fdc81cf22f and convert results to weatherInfo
+// Make a request from open weather api htts://api.openweathermap.org/data/2.5/weather?q={city}&appid=4da7e530effb30d411db83fdc81cf22f and convert results to weatherInfo section
 
 const searchButton = document.querySelector("#search");
 // Section where to append data from api
 const weatherInfo = document.querySelector(".weather-info");
-// Select Fahrenheit
+// Link Fahrenheit
 const imperial = document.querySelector("#imperial");
-// Select Celsius
+// Link Celsius
 const metric = document.querySelector("#metric");
 
 // Fetch weather data
 async function fetchData(units) {
+  // metadata to reuse in showWeatherData function
   const weatherUnits = {
     imperial: {
       degree: "F",
@@ -23,7 +24,7 @@ async function fetchData(units) {
   removeWeather();
   try {
     const textInput = document.querySelector("input").value;
-    const apiUrlFahrenheit = `http://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=4da7e530effb30d411db83fdc81cf22f&units=${units}`;
+    const apiUrlFahrenheit = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=4da7e530effb30d411db83fdc81cf22f&units=${units}`;
     let response = await axios.get(apiUrlFahrenheit);
     let weatherData = response.data;
     showWeatherData(weatherData, weatherUnits[units]);
@@ -41,7 +42,7 @@ async function fetchData(units) {
 function showWeatherData(data, metadata) {
   let weatherIcon = data.weather[0].icon;
   let weatherElements = `
-  <img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png"/>
+  <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png"/>
   <h1>${data.name} | ${data.sys.country}</h1>
   <h3>${data.weather[0].main}</h3>
   <p class="main-temp">${data.main.temp}&#176 ${metadata.degree}</p>
